de8afd2b0fcda436a70103de18680e1620641109
TalitaEvancely <evancely@gmail.com>
2022-01-21 17:49:52 -0300

    implementando projetos

--- novos projetos/projetos JS/CURSO JAVASCRIPT/Sintaxe e operadores/implementa.js
+++ novos projetos/projetos JS/CURSO JAVASCRIPT/Sintaxe e operadores/implementa.js
@@ -1,5 +1,13 @@
-     //Tentando fazer de uma outra forma para funcionar
-     
+/*Atividade
+Crie uma função que recebe dois números como parâmetros.
+- 1a parte: Confira se os números são iguais.
+- 2a parte: Confira se a soma dos números é maior que 10 ou menor que 20.
+- Retorne uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20".
+Exemplo:
+
+Input: 1, 2
+Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20.*/
+          
      var num1 = document.querySelector('input#n1');
      var num2 = document.querySelector('input#n2');
      var resp = document.querySelector('div#resp'); 
@@ -9,21 +17,29 @@ function verificar () {
      
     var num1=Number(n1.value)
      var num2=Number(n2.value) 
-     //saoIguais = num1===num2;
-   // return saoIguais ? "São Iguais" : "não são iguais" ;
-    var soma = num1 + num2;
-    document.write (`A soma de ${num1} e ${num2} é ${soma}`)
-    
-    switch (verificar) {
-        case num1 == num2:
-            document.write(`são iguais`)
-        break;
-        case num1 != num2:
-            document.write(`são diferentes`) 
-        break;
-        default:
-        document.write(`continua`)
+    //1a parte:
+    if (num1==num2){
+        document.write(`${num1} e ${num2} são . `)
+    } else{
+        document.write(`${num1} e ${num2} não são iguais.`)
     }
+    //2a parte:
+    const soma = num1 + num2;
+	let comparaDez = 'menor';
+	let comparaVinte = 'menor';
+
+	if (soma > 10) {
+		comparaDez = 'maior';
+	}
+
+	if (soma > 20) {
+		comparaVinte = 'maior';
+	}
+
+	document.write ( ` Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`);
+
+      
+    
         }
        
     
